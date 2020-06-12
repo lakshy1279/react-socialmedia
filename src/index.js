7964f122ca8b6d199fc93e1756fd1e85c8e975a1
lakshy pachory <luckyron1279@gmail.com>
2020-06-12 14:54:09 +0530

    fetch posts from api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { Provider } from 'react-redux';
 import { configureStore } from './store';
 const store = configureStore();
 console.log('state', store.getState());
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
--- src/reducers/post.js
+++ src/reducers/post.js
@@ -1,4 +1,10 @@
-export default function posts(state = []) {
+import { UPDATE_POSTS } from '../actions/actionTypes';
+export default function posts(state = [], action) {
   //{post:[]}
-  return state;
+  switch (action.type) {
+    case UPDATE_POSTS:
+      return action.posts;
+    default:
+      return state;
+  }
 }
