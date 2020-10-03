1e4b652c97e7a1dd2e2115da53d95c7c35b4e584
lakshy pachory <luckyron1279@gmail.com>
2020-10-03 22:31:55 +0530

    Updates

---
+++ precache-manifest.3fd2563e3c1105a512d974fcf03a391e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b757640c185cc069a99190692553ba0a",
+    "url": "/react-socialmedia/index.html"
+  },
+  {
+    "revision": "ca1e9959389113ddcf00",
+    "url": "/react-socialmedia/static/css/main.d4446d2d.chunk.css"
+  },
+  {
+    "revision": "e9e8da537bdabf30ad8a",
+    "url": "/react-socialmedia/static/js/2.2939e2c1.chunk.js"
+  },
+  {
+    "revision": "06d37f54fcda98f36f149637e2d89607",
+    "url": "/react-socialmedia/static/js/2.2939e2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1e9959389113ddcf00",
+    "url": "/react-socialmedia/static/js/main.9c738eee.chunk.js"
+  },
+  {
+    "revision": "db8f140d7ea73a802c60",
+    "url": "/react-socialmedia/static/js/runtime-main.9b42944e.js"
+  }
+]);
